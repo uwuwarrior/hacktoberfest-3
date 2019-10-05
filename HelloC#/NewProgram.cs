a454feaaec189f09ace308a6e414decbb5e41086
Aditya Kolla <aditya.169108011@muj.manipal.edu>
2019-10-05 11:26:06 +0530

    Hello C#

---
+++ HelloC#/NewProgram.cs
@@ -0,0 +1,15 @@
+using System; 
+  
+namespace HelloWorldApp { 
+      
+    class NewProgram { 
+          
+        static void Main(string[] args) { 
+              
+            // printing Hello World! 
+            Console.WriteLine("Hello World!"); 
+              
+            Console.ReadKey(); 
+        } 
+    } 
+} 
